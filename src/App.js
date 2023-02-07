edc8d2151d0a4a4fb130082f114938c6b5166dab
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2023-02-07 22:56:34 +0400

    Install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <div className="App">
+          <Navbar />
+          <section></section>
+        </div>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
