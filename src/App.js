bd8a5ef53dfd2e947f648566dc22c788c9659587
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2023-03-26 10:43:21 -0300

    Remove the bg color of html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import "./index.css";
+// import "./index.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
