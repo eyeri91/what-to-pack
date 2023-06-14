5817ff2bb24cce0a01ffe462c7765de44e3e1c40
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2023-06-14 21:12:15 +0400

    Display saved trip buttons in reverse order so that older trips get pushed down

--- src/Pages/Saved/Saved.js
+++ src/Pages/Saved/Saved.js
@@ -21,7 +21,7 @@ function Saved() {
 
   return (
     <div className="container saved-container col-md-6">
-      <div className="saved d-flex justify-content-center align-items-center flex-wrap">
+      <div className="saved d-flex flex-row-reverse justify-content-center align-items-center flex-wrap">
         {keysForSavedTrips
           ? createSavedTripButtons(keysForSavedTrips)
           : "The list is empty!"}
