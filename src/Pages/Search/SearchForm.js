d2e385ef8e3645f0d8df111d95446c52f570149b
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2023-06-16 10:30:35 +0400

    Convert loadOptions for locationAPI to async function

--- src/Pages/Search/SearchForm.js
+++ src/Pages/Search/SearchForm.js
@@ -5,23 +5,24 @@ import { GEO_API_URL, geoApiOptions } from "../../services/SearchLocationAPI";
 export default function SearchForm({ onSearchChange }) {
   const [location, setLocation] = useState(null);
 
-  const loadOptions = (input) => {
-    return fetch(
-      `${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${input}`,
-      geoApiOptions
-    )
-      .then((response) => response.json())
-      .then((response) => {
-        return {
-          options: response.data.map((city) => {
-            return {
-              value: `${city.latitude} ${city.longitude}`,
-              label: `${city.name} ${city.countryCode}`,
-            };
-          }),
-        };
-      })
-      .catch((err) => console.error(err));
+  const loadOptions = async (input) => {
+    try {
+      const response = await fetch(
+        `${GEO_API_URL}/cities?minPopulation=500000&namePrefix=${input}`,
+        geoApiOptions
+      );
+      const response_1 = await response.json();
+      return {
+        options: response_1.data.map((city) => {
+          return {
+            value: `${city.latitude} ${city.longitude}`,
+            label: `${city.name} ${city.countryCode}`,
+          };
+        }),
+      };
+    } catch (err) {
+      return console.error(err);
+    }
   };
 
   const handleOnChange = (searchInput) => {
