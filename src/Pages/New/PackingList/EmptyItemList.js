29068e83ee4eb8b3160ed18c26256f48fbde6326
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2023-06-17 18:56:51 +0400

    Add 'key' to the list element in EmptyItemList component

--- src/Pages/New/PackingList/EmptyItemList.js
+++ src/Pages/New/PackingList/EmptyItemList.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-export const EmptyItemList = () => {
+export const EmptyItemList = (props) => {
   return (
-    <li className="list-group-item packing-item ms-2">
+    <li className="list-group-item packing-item ms-2" key={props.key}>
       <span className="me-3 text-secondary d-flex justify-content-center">
         It's empty
       </span>
