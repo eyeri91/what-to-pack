b1c343ffcba67680b16a2ad83e3674faf51ce45f
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2023-06-17 19:54:58 +0400

    Style the deleteTripBtn and SavedListBtn

--- src/Pages/New/PackingList/ButtonsAndModal/SaveCurrentListBtn.js
+++ src/Pages/New/PackingList/ButtonsAndModal/SaveCurrentListBtn.js
@@ -9,25 +9,12 @@ export const SaveCurrentListBtn = () => {
       <SaveCurrentListModal id={saveListBtnId} />
       <button
         type="button"
-        className="save-list-btn rounded reset-btn d-flex align-items-center"
+        className="save-list-btn border-0 rounded-3 py-1 px-3 text-center"
         data-bs-toggle="modal"
         data-bs-target={`#${saveListBtnId}`}
         id={saveListBtnId}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          className="bi bi-arrow-up me-2"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fillRule="evenodd"
-            d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"
-          />
-        </svg>
-        Save this trip
+        Save
       </button>
     </>
   );
--- src/Pages/Saved/SavedTripsButtonsAndModal/DeleteTripBtn.js
+++ src/Pages/Saved/SavedTripsButtonsAndModal/DeleteTripBtn.js
@@ -9,25 +9,12 @@ export const DeleteTripBtn = (props) => {
       <DeleteTripModal tripKey={props.tripKey} id={deleteSavedTripBtnId} />
       <button
         type="button"
-        className="reset-btn delete-trip-btn d-flex align-items-center"
+        className="delete-trip-btn border-0 rounded-3 py-1 px-3 me-5"
         data-bs-toggle="modal"
         data-bs-target={`#${deleteSavedTripBtnId}`}
         id={deleteSavedTripBtnId}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          className="bi bi-arrow-up me-2"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fillRule="evenodd"
-            d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"
-          />
-        </svg>
-        Delete this trip
+        Delete
       </button>
     </>
   );
